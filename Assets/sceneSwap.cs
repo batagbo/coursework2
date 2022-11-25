7e21fe688a8eca3b52a5067aaec07da0641bebc4
batagbo <bata.gabo@gmail.com>
2022-11-25 12:30:48 +0100

    Initial commit

---
+++ Assets/sceneSwap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwap : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision Player)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
